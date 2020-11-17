707707f9788500fcaeb35e6ff2a607f928c95d22
JUAN DAVID NUÑEZ <JUANDAVID@JUANDAVID-MacBook-Pro.local>
2020-11-16 20:51:17 -0500

    DataTable Sin Funcion

---
+++ src/assets/js/main.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {    
+    $('#tablaApolonia').DataTable({      
+		//ESTA PARTE DEL CODIGO DE LENGUAJE SIRVE PARA QUE NUESTRA TABLA SE 
+		//LA PODAMOS VER EN DIFERENTES IDIOMAS   
+        language: {
+                "lengthMenu": "Mostrar _MENU_ registros",
+                "zeroRecords": "No se encontraron resultados",
+                "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+                "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+                "sSearch": "Buscar:",
+                "oPaginate": {
+                    "sFirst": "Primero",
+                    "sLast":"Último",
+                    "sNext":"Siguiente",
+                    "sPrevious": "Anterior"
+			     },
+			     "sProcessing":"Procesando...",
+			},
+			
+			
+        //CODIGO PARA USAR LOS BOTONES  
+        /*responsive: "true",
+        dom: 'Bfrtilp',       
+        buttons:[ 
+			{
+				extend:    'excelHtml5',
+				text:      '<i class="fas fa-file-excel"></i> ',
+				titleAttr: 'Exportar a Excel',
+				className: 'btn btn-success'
+			},
+			{
+				extend:    'pdfHtml5',
+				text:      '<i class="fas fa-file-pdf"></i> ',
+				titleAttr: 'Exportar a PDF',
+				className: 'btn btn-danger'
+			},
+			{
+				extend:    'print',
+				text:      '<i class="fa fa-print"></i> ',
+				titleAttr: 'Imprimir',
+				className: 'btn btn-info'
+			},
+		]*/	        
+    });     
+});
