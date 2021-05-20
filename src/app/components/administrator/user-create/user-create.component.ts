eee9cc2d6152ed3f5f92bb7c40532d833ebb7bba
Andres <acorozco@misena.edu.co>
2021-05-20 10:25:00 -0500

    Update Admin

--- src/app/components/administrator/user-create/user-create.component.ts
+++ src/app/components/administrator/user-create/user-create.component.ts
@@ -107,8 +107,8 @@ export class UserCreateComponent implements OnInit {
     this.datos=this.userForm.value;
     this.datos.ciudad = this.userForm.value.ciudad['ciudad'];
     this.datos.fecha_nacimiento = moment(this.userForm.value.fecha_nacimiento).format("YYYY-MM-DD");
-  this.administratorService.crearUsuario(this.datos).subscribe(resp=>{
-    console.log(resp)
+    this.administratorService.crearUsuario(this.datos).subscribe(resp=>{
+    console.log(this.userForm.value.ciudad['ciudad'])
     Swal.fire(
       'En hora buena',
       'Usuario creado correctamente!',
